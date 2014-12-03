9030bd7c9bbe24360b417d96af75008110d52b3f
peter-mcdonald <pea_mach@hotmail.com>
2014-12-03 14:24:10 +0800

    Bit of a fix up

--- UrbanMovement/Global.asax.cs
+++ UrbanMovement/Global.asax.cs
@@ -22,6 +22,7 @@ namespace UrbanMovement
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AuthConfig.RegisterAuth();
         }
     }
 }
--- UrbanMovement/Properties/AssemblyInfo.cs
+++ UrbanMovement/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8270efa9-7c62-4b6f-9182-47b2a3a96377")]
+[assembly: Guid("759fd709-8960-423e-939e-72c5e91c485a")]
 
 // Version information for an assembly consists of the following four values:
 //
