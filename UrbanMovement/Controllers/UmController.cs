84a6d73b394852f06ddfeb7023a78fc915e91b58
peter-mcdonald <pea_mach@hotmail.com>
2014-12-03 15:35:08 +0800

    Gettingstructures in place

---
+++ UrbanMovement/Controllers/UmController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace UrbanMovement.Controllers
+{
+    public class CofController : Controller
+    {
+        protected string RenderViewToString(string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+            {
+                throw new ArgumentNullException("viewName");
+            }
+
+            ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindView(ControllerContext, viewName, string.Empty);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+
+        protected string RenderPartialViewToString(string viewName, object model)
+        {
+            if (string.IsNullOrEmpty(viewName))
+            {
+                throw new ArgumentNullException("viewName");
+            }
+
+            ViewData.Model = model;
+
+            using (var sw = new StringWriter())
+            {
+                var viewResult = ViewEngines.Engines.FindPartialView(ControllerContext, viewName);
+                var viewContext = new ViewContext(ControllerContext, viewResult.View, ViewData, TempData, sw);
+                viewResult.View.Render(viewContext, sw);
+
+                return sw.GetStringBuilder().ToString();
+            }
+        }
+    }
+}
--- UrbanMovement/Global.asax.cs
+++ UrbanMovement/Global.asax.cs
@@ -22,7 +22,6 @@ namespace UrbanMovement
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }
